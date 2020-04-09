29dbfa46e713e0756f127c11985641c0aec5a812
Christian Hall <chris.ant.hall@gmail.com>
2020-04-09 00:06:32 -0700

    Small api update to pull in from linux vm.

--- restful_api/urls.py
+++ restful_api/urls.py
@@ -3,10 +3,14 @@ from rest_framework import routers
 
 from restful_api import views
 
+from rest_framework.authtoken import views as auth_views
+
+
 router = routers.DefaultRouter()
 router.register("users", views.UserViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path("api-token-auth/", auth_views.obtain_auth_token),
 ]
